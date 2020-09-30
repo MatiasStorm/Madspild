082434c89fadf7b0a934fc5af4d3499b1aafcf4c
MatiasStorm <mat-435@hotmail.com>
2020-09-30 13:49:29 +0200

    Add more recipes

--- madspild/src/main/java/com/example/demo/models/Recipes.java
+++ madspild/src/main/java/com/example/demo/models/Recipes.java
@@ -6,22 +6,10 @@ public class Recipes {
 
     ArrayList<Recipe> listOfRecipes = new ArrayList<>();
 
-    public void createRecipe(String name){
-        Recipe lasagne = new Recipe(name);
-        lasagne.addIngredient(new Ingredient("Ground beef", 500, "gram"));
-        lasagne.addIngredient(new Ingredient("Pasta", 500, "gram"));
-        lasagne.addIngredient(new Ingredient("Salt", 5, "gram"));
-        lasagne.addIngredient(new Ingredient("Onion", 2, "pieces"));
-        lasagne.addIngredient(new Ingredient("Copped tomatoes", 500, "gram"));
-        lasagne.addIngredient(new Ingredient("Carrots", 200, "gram"));
-        listOfRecipes.add(lasagne);
-    }
 
-    public Recipes() {
-        String[] names = {"Lagsagne", "Pizza", "Bøf bernaise", "Bacon and potatoes", "Tomatoes", "Dild"};
-        for(String name : names){
-            createRecipe(name);
-        }
+
+    public Recipes(ArrayList<Recipe> listOfRecipes) {
+        this.listOfRecipes = listOfRecipes;
     }
 
     public ArrayList<Recipe> getListOfRecipes(){
