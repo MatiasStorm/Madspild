4e07632d74e8f67eaece5536c73a5f2758f73fb9
MatiasStorm <mat-435@hotmail.com>
2020-10-01 10:20:11 +0200

    Add _blank to footer a tags

--- madspild/src/main/java/com/example/demo/controllers/MyController.java
+++ madspild/src/main/java/com/example/demo/controllers/MyController.java
@@ -5,13 +5,16 @@ import com.example.demo.models.Recipes;
 import com.example.demo.services.FoodPlanService;
 import com.example.demo.services.RecipeService;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 @Controller
@@ -60,8 +63,5 @@ public class MyController {
     @GetMapping("/organisation")
     public String organisation(){
         return "organisation";
-
     }
-
-
 }
